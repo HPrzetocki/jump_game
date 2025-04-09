df4574cc0fc51c6f93b604f7b5def2cecfb18c1b
HPrzetocki <116436436+HPrzetocki@users.noreply.github.com>
2025-04-09 20:45:08 +0200

    level_design1

--- Assets/Scripts/PlayerCon.cs
+++ Assets/Scripts/PlayerCon.cs
@@ -154,4 +154,18 @@ public class PlayerCon : MonoBehaviour
             isWalled = false;
         }
     }
+
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 offset = new Vector3(0, 0, -10);
+
+        void LateUpdate()
+        {
+            if (target != null)
+            {
+                transform.position = target.position + offset;
+            }
+        }
+    }
 }
