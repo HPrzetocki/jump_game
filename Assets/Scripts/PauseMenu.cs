18ee81293fd8c25f2518f11f7b65e87f007bbddc
HPrzetocki <116436436+HPrzetocki@users.noreply.github.com>
2025-11-29 17:57:28 +0100

    zmiana 2911
    1

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,27 +4,40 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) Resume();
+            else Pause();
+        }
+    }
 
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        isPaused = true;
     }
+
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
     }
+
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        isPaused = false;
     }
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
     }
-
-
 }
